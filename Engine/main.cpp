f3f77994510ca981df90c22fcda7b1e97c9f0765
Ratmir Gaitov <gaitovrat@gmail.com>
2022-11-17 20:02:54 +0100

    Initial Commit

---
+++ Engine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Engine
+//
+//  Created by Ratmir Gaitov on 17.11.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
