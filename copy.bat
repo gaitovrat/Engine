e28fc1a9f223b02a778a063e6b6c97208631a555
Ratmir Gaitov <gaitovrat@gmail.com>
2022-11-18 20:04:14 +0100

    SLN

---
+++ copy.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set build_dir=%1
+set pwd=%~dp0
+
+if "%build_dir%"=="" goto end
+
+mkdir %build_dir%
+copy /Y "%pwd%library\glfw\lib-vc2022\glfw3.dll" "%build_dir%\glfw3.dll"
+copy /Y "%pwd%library\glew\bin\Release\x64\glew32.dll" "%build_dir%\glew32.dll"
+
+xcopy /SYI "%pwd%engine\assets" "%build_dir%\assets"
+
+:end
