26757f17982144e91ceb50efd38c641873bc908b
Ratmir Gaitov <gaitovrat@gmail.com>
2022-12-02 17:34:11 +0100

    Add libraries

--- copy.bat
+++ copy.bat
@@ -6,9 +6,9 @@ set pwd=%~dp0
 if "%build_dir%"=="" goto end
 
 mkdir %build_dir%
-copy /Y "%pwd%library\glfw\lib-vc2022\glfw3.dll" "%build_dir%\glfw3.dll"
-copy /Y "%pwd%library\glew\bin\Release\x64\glew32.dll" "%build_dir%\glew32.dll"
-copy /Y "%pwd%library\assimp\build\bin\Debug\assimp-vc143-mtd.dll" "%build_dir%\assimp-vc143-mtd.dll"
+copy /Y "%pwd%library\glfw\lib\glfw3.dll" "%build_dir%\glfw3.dll"
+copy /Y "%pwd%library\glew\bin\glew32.dll" "%build_dir%\glew32.dll"
+copy /Y "%pwd%library\assimp\bin\assimp-vc143-mt.dll" "%build_dir%\assimp-vc143-mt.dll"
 
 xcopy /SYI "%pwd%engine\assets" "%build_dir%\assets"
 
--- library/assimp/build/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ratmir/Desktop/engine/library/assimp/contrib/zlib
-# Build directory: C:/Users/ratmir/Desktop/engine/library/assimp/build/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
